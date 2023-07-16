dd483d3b7a21c3a6fa7f80a8d59d6fae427255f5
Jhonathan Costa <jhonathan_jcs@hotmail.com>
2023-07-15 21:49:02 -0400

    contact
    done

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,6 +1,66 @@
+import { useState } from 'react';
+import swal from 'sweetalert';
+
 function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [resume, setResume] = useState('');
+
+    function resetForm() {
+    setName('');
+    setEmail('');
+    setResume('');
+    setPhone('');
+    }
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+        void swal({
+            title: "Obrigado! Seu email foi enviado com sucesso. 😊",
+            text: "Em breve estarei entrando em contato.",
+            icon: "success",
+        });
+        resetForm()
+    } 
+
     return(
-        <h1>Contact</h1>
+        <><h1>Contact</h1><div>
+            <form onSubmit={(event) => handleSubmit(event)}>
+                <label>
+                    Nome
+                    <input
+                        placeholder='Digite seu nome'
+                        required
+                        value={name}
+                        onChange={({ target }) => setName(target.value)} />
+                </label>
+                <label>
+                    E-mail
+                    <input
+                        placeholder='Digite seu email'
+                        required
+                        value={email}
+                        onChange={({ target }) => setEmail(target.value)} />
+                </label>
+                <label>
+                    Celular(opcional)
+                    <input
+                        placeholder='Digite o numero do seu celular'
+                        value={phone}
+                        onChange={({ target }) => setPhone(target.value)} />
+                </label>
+                <label>
+                    Mensagem
+                    <textarea
+                        placeholder='Digite sua mensagem...'
+                        required
+                        value={resume}
+                        onChange={({ target }) => setResume(target.value)} />
+                </label>
+                <button>Enviar</button>
+            </form>
+        </div></>
     )
 }
 
--- src/pages/home/imgs/imagem.tsx
+++ src/pages/home/imgs/imagem.tsx
@@ -1,9 +1,10 @@
 import './imagem.css'
+import img from '../../../assets/photo-me.jpg'
 
 function Img() {
     return(
         <div className="img-me">
-            <img src="https://media.licdn.com/dms/image/C4D03AQE8eVquX0smyg/profile-displayphoto-shrink_800_800/0/1655208312060?e=1694649600&v=beta&t=t2jB5MYb5Qr33dvTN_h_rFRuP4SnG_2o_wh5KEw_ReU" alt="me-photo" />
+            <img src={img} alt="me-photo" />
         </div>
     )
 }
